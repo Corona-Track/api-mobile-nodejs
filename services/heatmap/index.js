456c639aae360dd2acf9065741875dc90ee8c2f4
lourencogtac <lourenco@gtacsolutions.com>
2020-05-07 21:58:15 -0300

    Firebase integration has been added

--- services/heatmap/index.js
+++ services/heatmap/index.js
@@ -1,28 +1,31 @@
 'use strict'
 
-import * as admin from 'firebase-admin';
-import HeatMapService from './heatmapservice';
+const admin = require('firebase-admin');
+const HeatMapService = require('./heatmapservice');
+
+var serviceAccount = require('../../config/coronatrackrn-firebase-adminsdk.json');
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://coronatrackrn.firebaseio.com'
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://coronatrackrn.firebaseio.com"
 });
 
 const firestore = admin.firestore();
 
 // If you prefer async/await, use the following
 //
-module.exports = async function (fastify, opts) {
-  fastify.get('/heatmap/getMapElementsByPosition', async function (req, res) {
+module.exports = async function (fastify, opts, next) {
+  fastify.post('/heatmap/getMapElementsByPosition', async function (req, res) {
     try {
-      let { markerCentral, markerNorthWest, markerSouthWest, markerNorthEast, markerSouthEast } = req.body;
       //Check for null because it can use negative values
-      if (!(markerCentral !== null &&
-        markerNorthWest !== null &&
-        markerSouthWest !== null &&
-        markerNorthEast !== null &&
-        markerSouthEast !== null))
-        return res.sendStatus(500);
+      if (!(req.body.markerCentral !== null &&
+        req.body.markerNorthWest !== null &&
+        req.body.markerSouthWest !== null &&
+        req.body.markerNorthEast !== null &&
+        req.body.markerSouthEast !== null))
+        return res.status(404);
+      let { markerCentral, markerNorthWest, markerSouthWest, markerNorthEast, markerSouthEast } = req.body;
+
       let region = {
         markerCentral,
         markerNorthWest,
@@ -37,9 +40,44 @@ module.exports = async function (fastify, opts) {
       let squaresToCalculate = HeatMapService.processGridSquares(gridSquares);
       let calculatedSquares = HeatMapService.calculateSquares(squaresToCalculate);
       return res.status(200).send(JSON.stringify(calculatedSquares));
-    } catch (e) { return res.statusCode(500); }
+    } catch (e) { res.status(500).send(e) }
   })
+
+  next()
 }
+
+
+// module.exports = async function (fastify, opts) {
+//   fastify.get('/heatmap/getMapElementsByPosition', async function (req, res) {
+//     try {
+//       if (!req.body)
+//         return res.status(404);
+//       //Check for null because it can use negative values
+//       if (!(markerCentral !== null &&
+//         markerNorthWest !== null &&
+//         markerSouthWest !== null &&
+//         markerNorthEast !== null &&
+//         markerSouthEast !== null))
+//         return res.status(404);
+//       let { markerCentral, markerNorthWest, markerSouthWest, markerNorthEast, markerSouthEast } = req.body;
+
+//       let region = {
+//         markerCentral,
+//         markerNorthWest,
+//         markerSouthWest,
+//         markerNorthEast,
+//         markerSouthEast
+//       };
+//       let users = await getUsersInsideRange(region);
+//       let citiesContent = await getAllCities(region);
+//       let convertedUsers = HeatMapService.populateUserCity(citiesContent.allCities, users);
+//       let gridSquares = HeatMapService.generateGrid(region, citiesContent, convertedUsers);
+//       let squaresToCalculate = HeatMapService.processGridSquares(gridSquares);
+//       let calculatedSquares = HeatMapService.calculateSquares(squaresToCalculate);
+//       return res.status(200).send(JSON.stringify(calculatedSquares));
+//     } catch (e) { res.status(500).send(e) }
+//   })
+// }
 
 
 const getUsersInsideRange = async region => {
